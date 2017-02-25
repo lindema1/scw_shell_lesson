bfa83539ca8c118cbcd5a947924024a1c20ed2d5
lindema1 <lindema1@email.arizona.edu>
2017-02-25 12:20:40 -0700

    add help message to the script

--- first_script.sh
+++ first_script.sh
@@ -1,2 +1,8 @@
+#added user message 
+if [ "$1" == "-h" ] || [ "$1"=="--help" ]; then
+	echo "usage: sh $0 input_file output_file"
+	exit
+fi
+
 # this script counts characters in a file
 head -5 $1 | sort | wc -c  > $2
